17f1d3f996dfbb20e96934636f68cd1a45ceb485
Radhika S <radhika.s@tvsnext.io>
2021-08-12 13:08:21 +0530

    [Radhika] Refactor: Calculate Employee Wage Using Function

--- Usecases/EmployeeWage.js
+++ Usecases/EmployeeWage.js
@@ -2,7 +2,7 @@
 //UC1
 const IS_ABSENT=0;
 {
-let empCheck=Math.floor(Math.random()*10)%2;
+var empCheck=Math.floor(Math.random()*10)%2;
 if(empCheck==IS_ABSENT)
 {
     console.log("UC1--->Employee is Absent");
@@ -22,21 +22,55 @@ const WAGE_PER_HR=20;
 {
 let empHrs=0;
 let empWage=0;
-let empCheck1=Math.floor(Math.random()*10)%3;
-switch(empCheck1)
+var empCheck=Math.floor(Math.random()*10)%3;
+switch(empCheck)
 {
     case IS_PART_TIME:
         empHrs=PART_TIME_HRS;
-        console.log("UC2-->Part time employee "+empHrs);
+        console.log("UC2-->Part time employee: "+empHrs);
         break;
         case IS_FULL_TIME:
             empHrs=FULL_TIME_HRS;
-            console.log("UC2--->Full time employee "+empHrs);
+            console.log("UC2--->Full time employee :"+empHrs);
             break;
             default:
                 empHrs=0;
                 console.log("UC2--->Employee is absent "+empHrs);
 }
 empWage=empHrs*WAGE_PER_HR;
-console.log("Emp Wage : "+empWage);
+console.log("UC2--->Emp Wage : "+empWage);
 }
+
+//UC3--->Refactor the UC2 Using Function
+const PART_TIME=1;
+const FULL_TIME=2;
+const PART_TIME_HRS1=4;
+const FULL_TIME_HRS1=8;
+const WAGE_PER_HR1=20;
+{
+
+var empCheck=Math.floor(Math.random()*10)%3;
+function getWorkinghours(empCheck)
+{
+switch(empCheck)
+{
+    case PART_TIME:
+        empHrs=PART_TIME_HRS1;
+        console.log("UC3-->Part time employee: " +empHrs);
+        break;
+        case FULL_TIME:
+            empHrs=FULL_TIME_HRS1;
+            console.log("UC3--->Full time employee :" +empHrs);
+            break;
+            default:
+                empHrs=0;
+                console.log("UC3--->Employee is absent " +empHrs);
+}
+}
+let empHrs1=0;
+let empWage1=0;
+empHrs1=getWorkinghours(empCheck);
+empWage1=empHrs*WAGE_PER_HR1;
+console.log("UC3--->Emp Wage : "+empWage1);
+}
+

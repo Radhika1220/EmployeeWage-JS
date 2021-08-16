efe4ff6c98abf6285abdad18f6ff9063f4a0373f
Radhika S <radhika.s@tvsnext.io>
2021-08-16 13:37:49 +0530

    [Radhika] Add:Adding the gender and startdate property

--- Usecases/EmployeePayroll.js
+++ Usecases/EmployeePayroll.js
@@ -6,12 +6,17 @@ class EmployeePayrollData
     id;
     name;
     salary;
+    //adding the property of gender and startdate
+    gender;
+    startDate;
     //defining constructor
-    constructor(id,name,salary)
+    constructor(...params)
     {
-        this.id=id;
-        this.name=name;
-        this.salary=salary;
+        this.id=params[0];
+        this.name=params[1];
+        this.salary=params[2];
+        this.gender=params[3];
+        this.startDate=params[4];
     }
     //getter and setter methods
     get name()
@@ -33,15 +38,24 @@ class EmployeePayrollData
     //tostring method
     toString()
     {
+        return "id="+this.id + " name="+this.name + " salary="+this.salary + " Gender="+this.gender +" Date="+this.startDate;
+    }
+    toString1()
+    {
         return "id="+this.id + " name="+this.name + " salary="+this.salary;
     }
 }
 //creating a object for class and passing the parameter(calling constructor)
 let employee=new EmployeePayrollData(1,"Radhika",40000);
 console.log("Calling constructor and access the property")
-console.log(employee.toString());
-console.log("Change the property usin getter and setter method");
+console.log(employee.toString1());
+
+console.log("Change the property using getter and setter method");
 //change the name using getter and setter method
 employee.name="Priya";
 employee.salary="50000"
-console.log(employee.toString());
+console.log(employee.toString1());
+
+let employee1=new EmployeePayrollData(2,"VishnuPriya",55000,'F',new Date());
+console.log(employee1.toString());
+
